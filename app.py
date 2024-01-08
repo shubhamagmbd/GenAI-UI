6702e031568bd321f3299b689403f96c001e18c3
shubhamagmbd <155981810+shubhamagmbd@users.noreply.github.com>
2024-01-08 20:42:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,13 @@
+#Note: The openai-python library support for Azure OpenAI is in preview.
 import os
+import openai
+openai.api_type = "azure"
+openai.api_base = "https://rcgth-hackathon-aoai-eus.openai.azure.com/"
+openai.api_version = "2023-06-01-preview"
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
-from flask import (Flask, redirect, render_template, request,
-                   send_from_directory, url_for)
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def index():
-   print('Request for index page received')
-   return render_template('index.html')
-
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
-
-@app.route('/hello', methods=['POST'])
-def hello():
-   name = request.form.get('name')
-
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
-
-
-if __name__ == '__main__':
-   app.run()
+response = openai.Image.create(
+    prompt='water bottle',
+    size='1024x1024',
+    n=1
+)
