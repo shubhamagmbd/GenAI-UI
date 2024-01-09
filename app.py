7c7181188012f0d18da65da5c571e568ac5eff1d
shubhamagmbd <155981810+shubhamagmbd@users.noreply.github.com>
2024-01-09 12:53:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 import os
-from openai import AzureOpenAI
-import requests
-from PIL import Image
-import json
 
 from flask import (Flask, redirect, render_template, request,
                    send_from_directory, url_for)
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
@@ -17,5 +15,18 @@ def index():
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+@app.route('/hello', methods=['POST'])
+def hello():
+   name = request.form.get('name')
+
+   if name:
+       print('Request for hello page received with name=%s' % name)
+       return render_template('hello.html', name = name)
+   else:
+       print('Request for hello page received with no name or blank name -- redirecting')
+       return redirect(url_for('index'))
+
+
 if __name__ == '__main__':
    app.run()
