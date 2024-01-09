0be53601fea02c8beef2f957d9060755e7ae3b7e
shubhamagmbd <155981810+shubhamagmbd@users.noreply.github.com>
2024-01-09 13:26:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 import os
+from openai import AzureOpenAI
+import requests
+from PIL import Image
+import json
 
 from flask import (Flask, redirect, render_template, request,
                    send_from_directory, url_for)
@@ -19,6 +23,11 @@ def favicon():
 @app.route('/hello', methods=['POST'])
 def hello():
    name = request.form.get('name')
+   client = AzureOpenAI(
+      api_version="2023-12-01-preview",  
+      api_key='c7a1c79eb07d4ff380f09938640f774c',  
+      azure_endpoint='https://rcgth-hackathon-aoai-eu2.openai.azure.com/'
+   )
 
    if name:
        print('Request for hello page received with name=%s' % name)
